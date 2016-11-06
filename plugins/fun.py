641974fd35bb5019153f997dc25b3a8eaf5adda8
Lee Watson <rev@revthefox.co.uk>
2016-11-06 13:33:27 +0000

    Add whatthecommit to fun.py

--- plugins/fun.py
+++ plugins/fun.py
@@ -12,7 +12,7 @@ class Plugin(object):
 
     @command(permission='view')
     def insult(self, mask, target, args):
-        """Send a randomly generated insult.
+        """Send a randomly generated insult from insultgenerator.org
 
             %%insult
         """
@@ -26,6 +26,26 @@ class Plugin(object):
             doc = html.fromstring(response.text)
             insult = ''.join(doc.xpath('//div[@class="wrap"]/text()')).strip()
             yield insult
+        finally:
+            if response is not None:
+                response.close()
+
+    @command(name='wtc', permission='view')
+    def whatthecommit(self, mask, target, args):
+        """Send a random commit message from whatthecommit.com.
+
+            %%wtc
+        """
+        response = None
+        try:
+            response = requests.get('http://whatthecommit.com')
+        except requests.exceptions.RequestException as ex:
+            self.bot.log.exception(ex)
+            yield 'Error: {0}'.format(ex.strerror)
+        else:
+            doc = html.fromstring(response.text)
+            commit_message = doc.xpath('//div[@id="content"]/p/text()')[0].strip()
+            yield commit_message
         finally:
             if response is not None:
                 response.close()
