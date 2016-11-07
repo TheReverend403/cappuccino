08789a9818c56a0eaa71e26bdee857a61e0fd42d
Lee Watson <rev@revthefox.co.uk>
2016-11-07 17:54:15 +0000

    Add an existential crisis

--- plugins/ricedb.py
+++ plugins/ricedb.py
@@ -1,4 +1,5 @@
 import inspect
+import random
 
 import pylast
 import requests
@@ -111,7 +112,9 @@ class Plugin(object):
                 indexed_values.append('[{0}] {1}'.format(to_user_index(index), item))
             return '{0} [{1}]'.format(' | '.join(indexed_values), self.bot.antiping(user))
         else:
-            return '{0} no {1}.'.format(self.bot.antiping(user) + ' has' if user != mask.nick else 'You have', mode)
+            return '{0} no {1}.'.format(
+                self.bot.antiping(user) + ' has' if user != mask.nick else 'You have',
+                'reason to live' if random.random() <= 0.15 else mode)
 
     @command(permission='view')
     def dtop(self, mask, target, args):
