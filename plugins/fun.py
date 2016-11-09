955ab60730af5df17234726e22fddef972235316
Lee Watson <rev@revthefox.co.uk>
2016-11-09 15:44:32 +0000

    Fix regex in fun.py and automatically close HTTP requests

--- plugins/fun.py
+++ plugins/fun.py
@@ -1,7 +1,15 @@
+from contextlib import closing
+
 import irc3
 import requests
 from lxml import html
 from irc3.plugins.command import command
+
+USER_AGENT = 'ricedb/fun.py (https://github.com/TheReverend403/ricedb)'
+DEFAULT_HEADERS = {
+    'User-Agent': USER_AGENT,
+    'Accept-Language': 'en-GB, en-US, en'
+}
 
 
 @irc3.plugin
@@ -17,49 +25,32 @@ class Fun(object):
             %%insult
         """
         try:
-            response = requests.get('http://www.insultgenerator.org', headers={'Connection': 'close'})
+            with closing(requests.get('http://www.insultgenerator.org', headers={'Connection': 'close'})) as response:
+                doc = html.fromstring(response.text)
+                insult = ''.join(doc.xpath('//div[@class="wrap"]/text()')).strip()
+                yield insult
         except requests.exceptions.RequestException as ex:
             self.bot.log.exception(ex)
             yield 'Error: {0}'.format(ex.strerror)
-        else:
-            doc = html.fromstring(response.text)
-            insult = ''.join(doc.xpath('//div[@class="wrap"]/text()')).strip()
-            yield insult
 
-    @command(name='wtc', permission='view')
-    def whatthecommit(self, mask, target, args):
-        """Send a random commit message from whatthecommit.com.
-
-            %%wtc
-        """
-        try:
-            response = requests.get('http://whatthecommit.com', headers={'Connection': 'close'})
-        except requests.exceptions.RequestException as ex:
-            self.bot.log.exception(ex)
-            yield 'Error: {0}'.format(ex.strerror)
-        else:
-            doc = html.fromstring(response.text)
-            commit_message = doc.xpath('//div[@id="content"]/p/text()')[0].strip()
-            yield commit_message
-
-    @irc3.event(r'PRIVMSG (?P<target>#\S+) :\s*\[(?P<data>[A-Za-z0-9-_ \'"!]+)\]$')
+    @irc3.event(r'.*PRIVMSG (?P<target>#\S+) :\s*\[(?P<data>[A-Za-z0-9-_ \'"!]+)\]$')
     def intensify(self, target, data):
         self.bot.privmsg(target, self.bot.bold('[{0} INTENSIFIES]'.format(data.strip().upper())))
 
-    @irc3.event(r'PRIVMSG (?P<target>#\S+) :\s*wew$')
-    def wew(self, mask, target):
+    @irc3.event(r'.*PRIVMSG (?P<target>#\S+) :\s*wew$')
+    def wew(self, target):
         self.bot.privmsg(target, self.bot.bold('w e w l a d'))
 
-    @irc3.event(r'PRIVMSG (?P<target>#\S+) :\s*ayy+$')
-    def ayy(self, mask, target):
+    @irc3.event(r'.*PRIVMSG (?P<target>#\S+) :\s*ayy+$')
+    def ayy(self, target):
         self.bot.privmsg(target, 'lmao')
 
-    @irc3.event(r'PRIVMSG (?P<target>#\S+) :\s*(wh?(aa*z*|u)t?(\'?| i)s? ?up|\'?sup)$')
-    def gravity(self, mask, target):
+    @irc3.event(r'.*PRIVMSG (?P<target>#\S+) :\s*(wh?(aa*z*|u)t?(\'?| i)s? ?up|\'?sup)$')
+    def gravity(self, target):
         self.bot.privmsg(target,
                          '{0}: A direction away from the center of gravity of a celestial object.'.format(mask.nick))
 
-    @irc3.event(r'PRIVMSG (?P<target>#\S+) :\s*same$')
-    def same(self, mask, target):
+    @irc3.event(r'.*PRIVMSG (?P<target>#\S+) :\s*same$')
+    def same(self, target):
         self.bot.privmsg(target, self.bot.bold('same'))
 
