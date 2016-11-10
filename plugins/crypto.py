01c32644ae4eab7e1e4cd98148465d2214247c48
Lee Watson <rev@revthefox.co.uk>
2016-11-10 11:25:18 +0000

    Add crypto.py

---
+++ plugins/crypto.py
@@ -0,0 +1,31 @@
+import hashlib
+import irc3
+from irc3.plugins.command import command
+
+
+@irc3.plugin
+class Crypto(object):
+
+    requires = [
+        'plugins.formatting'
+    ]
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(permission='view')
+    def hash(self, mask, target, args):
+        """Hash text.
+
+            %%hash (--md5 | --sha1 | --sha256 | --sha512) <string>...
+        """
+        available_algorithms = hashlib.algorithms_guaranteed
+        text = ' '.join(args['<string>']).encode('UTF-8')
+        for algo in available_algorithms:
+            flag = '--{0}'.format(algo)
+            if flag in args and args[flag]:
+                hash_object = hashlib.new(algo)
+                hash_object.update(text)
+                return '{0}: {1}'.format(self.bot.antiping(mask.nick), hash_object.hexdigest())
+
+
