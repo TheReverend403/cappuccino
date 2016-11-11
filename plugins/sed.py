9168a773e8514be43035bd8f6a2f77b8e1bd2abe
Lee Watson <rev@revthefox.co.uk>
2016-11-11 13:04:51 +0000

    Fix sed loop not returning

--- plugins/sed.py
+++ plugins/sed.py
@@ -126,3 +126,4 @@ class Sed(object):
                 return
             self.bot.privmsg(target, '{0} thinks {1} {2} to say: {3}'.format(
                 self.bot.antiping(mask.nick), user, emphasised_meant, new_message))
+            return
