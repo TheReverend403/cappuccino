b6e3889be03a20d3c721f7a88aa01f7e3dec2f9d
Lee Watson <rev@revthefox.co.uk>
2016-11-28 14:19:12 +0000

    lastfm.py: remove track URLs, the additional HTTP request makes response times too long.

--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -1,8 +1,6 @@
 import irc3
 import pylast
 from irc3.plugins.command import command
-from pyshorteners import Shortener
-from pyshorteners.exceptions import UnknownShortenerException, ExpandingErrorException, ShorteningErrorException
 
 MAX_TRACK_ARTIST_LEN = 32
 MAX_TRACK_TITLE_LEN = 75
@@ -24,7 +22,6 @@ class LastFM(object):
         except KeyError:
             self.bot.log.warn('Missing last.fm API key')
             return
-        self.url_shortener = Shortener('Isgd', timeout=3)
 
     @command(name='np', permission='view')
     def now_playing(self, mask, target, args):
@@ -63,22 +60,15 @@ class LastFM(object):
             if not current_track:
                 return '{0} is not listening to anything right now.'.format(self.bot.format(irc_username, antiping=True))
 
-            track_url = current_track.get_url()
-            try:
-                track_url = self.url_shortener.short(track_url)
-            except (UnknownShortenerException, ShorteningErrorException, ExpandingErrorException) as err:
-                self.bot.log.exception('Exception occurred while shortening {0}: {1}'.format(track_url, err))
-
             artist = current_track.get_artist().get_name().strip()
             title = current_track.get_title().strip()
+
             if len(artist) > MAX_TRACK_ARTIST_LEN:
                 artist = ''.join(artist[:MAX_TRACK_ARTIST_LEN]) + '...'
             if len(title) > MAX_TRACK_TITLE_LEN:
                 title = ''.join(title[:MAX_TRACK_TITLE_LEN]) + '...'
-            track_info = '{0} - {1} | {2}'.format(
-                self.bot.format(artist, bold=True),
-                self.bot.format(title, bold=True),
-                self.bot.format(track_url, color=self.bot.color.BLUE))
+
+            track_info = '{0} - {1}'.format(self.bot.format(artist, bold=True), self.bot.format(title, bold=True))
         except pylast.NetworkError as err:
             return err
 
