c8c8b212bee5c81cdf2b65b4c736932a7571942b
Lee Watson <rev@revthefox.co.uk>
2018-01-16 09:19:48 +0000

    Remove bottle server

--- plugins/userdb.py
+++ plugins/userdb.py
@@ -4,9 +4,6 @@ except ImportError:
     import json
 
 import os
-import threading
-
-import bottle
 import irc3
 
 
@@ -14,56 +11,18 @@ class UserDB(dict):
     def __init__(self, bot, **kwargs):
         super().__init__(**kwargs)
 
-        self.bot = bot
-        self.file = os.path.join('data', 'ricedb.json')
+        self.__bot = bot
+        self.__file = os.path.join('data', 'ricedb.json')
 
-        datadir = os.path.dirname(self.file)
+        datadir = os.path.dirname(self.__file)
         try:
-            with open(self.file, 'r') as fd:
+            with open(self.__file, 'r') as fd:
                 self.update(json.load(fd))
         except FileNotFoundError:
             # Database file itself doesn't need to exist on first run, it will be created on first write.
             if not os.path.exists(datadir):
                 os.mkdir(datadir)
-                self.bot.log.debug('Created {0}/ directory'.format(datadir))
-
-        try:
-            self.config = self.bot.config[__name__]
-            if not self.config.get('enable_http_server'):
-                return
-            host, port = self.config['http_host'], int(self.config['http_port'])
-        except KeyError:
-            host, port = '127.0.0.1', 8080
-
-        bottle.hook('before_request')(self.__strip_path)
-        bottle.route('/')(self.__http_index)
-        bottle.route('/<user>')(self.__http_index)
-        bottle.route('/<user>/<key>')(self.__http_index)
-
-        bottle_thread = threading.Thread(
-            target=bottle.run,
-            kwargs={'quiet': True, 'host': host, 'port': port},
-            name='{0} HTTP server'.format(__name__),
-            daemon=True
-        )
-
-        bottle_thread.start()
-        self.bot.log.info('{0} started on http://{1}:{2}'.format(bottle_thread.name, host, port))
-
-    def __strip_path(self):
-        bottle.request.environ['PATH_INFO'] = bottle.request.environ['PATH_INFO'].rstrip('/')
-
-    def __http_index(self, user=None, key=None):
-        bottle.response.content_type = 'application/json'
-        if user:
-            if user not in self:
-                return bottle.HTTPResponse(status=404, body=json.dumps(None))
-            if key:
-                if key not in self.get(user):
-                    return bottle.HTTPResponse(status=404, body=json.dumps(None))
-                return json.dumps(self.get(user).get(key))
-            return json.dumps(self.get(user))
-        return json.dumps(self)
+                self.__bot.log.debug('Created {0}/ directory'.format(datadir))
 
     @irc3.extend
     def get_user_value(self, username, key):
@@ -79,5 +38,5 @@ class UserDB(dict):
             self[username].update(data)
         except KeyError:
             self[username] = data
-        with open(self.file, 'w') as fd:
+        with open(self.__file, 'w') as fd:
             json.dump(self, fd)
