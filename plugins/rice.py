3e8d160f797354f511918efc7c5cf7ac4270003e
Lee Watson <rev@revthefox.co.uk>
2018-10-30 19:33:41 +0000

    Make error message a little more helpful.

--- plugins/rice.py
+++ plugins/rice.py
@@ -93,7 +93,7 @@ class Rice(object):
             replacement = args['<value>'].strip()
 
             if not replacement:
-                return 'Replacement cannot be empty!'
+                return 'Please specify a replacement. If you\'re trying to delete an item, use -d/--delete instead.'
 
             values = self.bot.get_user_value(mask.nick, mode)
             if not values:
