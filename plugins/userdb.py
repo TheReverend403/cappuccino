5c986c594005c6b43c27be84e1d51f47662a4ed4
Lee Watson <rev@revthefox.co.uk>
2019-09-19 00:49:37 +0100

    Change log level of userdb syncs to debug.

--- plugins/userdb.py
+++ plugins/userdb.py
@@ -81,7 +81,7 @@ class UserDB(object):
             with self.file.open('w') as fd:
                 json.dump(self.data, fd)
             self.last_write = datetime.now()
-            self.bot.log.info('Synced database to disk.')
+            self.bot.log.debug('Synced database to disk.')
 
     def _shutdown_hook(self, *args):
         self.sync(force=True)
