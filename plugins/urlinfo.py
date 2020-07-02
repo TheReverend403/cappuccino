4041bddc1be408842e5e1ef5e128beba6a1cf070
Lee Watson <rev@revthefox.co.uk>
2020-07-02 19:57:51 +0100

    Refactor most of urlinfo.py to move functions into the plugin class.

--- plugins/urlinfo.py
+++ plugins/urlinfo.py
@@ -31,17 +31,6 @@ from humanize import naturalsize
 
 from util.formatting import Color, style, unstyle
 
-_URL_FINDER = re.compile(r'(?:https?://\S+)', re.IGNORECASE | re.UNICODE)
-_BRACES = [('{', '}'), ('<', '>'), ('[', ']'), ('(', ')')]
-_DEFAULT_MAX_BYTES = 10485760  # 10M
-_MAX_TITLE_LENGTH = 128
-_REQUEST_TIMEOUT = 5
-_HOSTNAME_CLEANUP_REGEX = re.compile(r'^www\.', re.IGNORECASE | re.UNICODE)
-_FORCE_IPV4_HOSTNAMES = ['www.youtube.com', 'youtube.com', 'youtu.be']
-_HTML_MIMETYPES = ['text/html', 'application/xhtml+xml']
-_REQUEST_CHUNK_SIZE = 1024  # Bytes
-_ALLOWED_CONTENT_TYPES = ['text', 'video', 'application']
-
 
 class ResponseBodyTooLarge(requests.RequestException):
     pass
@@ -59,110 +48,11 @@ class RequestTimeout(requests.RequestException):
     pass
 
 
-original_getaddrinfo = socket.getaddrinfo
-
-
-def _getaddrinfo_wrapper(host, port, family=0, type=0, proto=0, flags=0):
-    """
-    Some sites like YouTube like to block certain IPv6 ranges, so forcing IPv4 is necessary to get info on those URLs.
-    Because neither requests, urllib, or HTTPRequest provide a way to do that, it's necessary to bypass them and
-    go straight to the Python socket library and wrap it's getaddrinfo function transparently to return only
-    IPv4 addresses for certain hosts.
-
-    This function requires `socket.getaddrinfo = getaddrinfo_wrapper` somewhere early in a script's startup,
-    preferably before any network requests are made.
-    """
-    family = socket.AF_INET if host in _FORCE_IPV4_HOSTNAMES else family
-    return original_getaddrinfo(host, port, family, type, proto, flags)
-
-
-def _read_stream(response: requests.Response, max_bytes: int = _DEFAULT_MAX_BYTES) -> str:
-    start_time = time.time()
-    content = StringIO()
-
-    for chunk in response.iter_content(_REQUEST_CHUNK_SIZE):
-        if time.time() - start_time >= _REQUEST_TIMEOUT:
-            raise RequestTimeout(f'Request timed out ({_REQUEST_TIMEOUT} seconds).')
-        if not chunk:  # filter out keep-alive new chunks
-            continue
-        content_length = content.write(chunk.decode('UTF-8', errors='ignore'))
-        if content_length > max_bytes:
-            size = naturalsize(content_length, gnu=True)
-            raise ResponseBodyTooLarge(f'Couldn\'t find the page title within {size}.')
-
-    return content.getvalue()
-
-
-def _process_url(url: str, session):
-    hostname = urlparse(url).hostname
-    for (_, _, _, _, sockaddr) in socket.getaddrinfo(hostname, None):
-        ip = ipaddress.ip_address(sockaddr[0])
-        if not ip.is_global:
-            raise InvalidIPAddress(f'{hostname} is not a publicly routable address.')
-
-    hostname = _HOSTNAME_CLEANUP_REGEX.sub('', hostname)
-    with session.get(url, stream=True) as response:
-        if response.status_code != requests.codes.ok:
-            response.raise_for_status()
-
-        content_type = response.headers.get('Content-Type')
-        if content_type:
-            content_type, _ = cgi.parse_header(content_type)
-            main_type = content_type.split('/')[0]
-            if main_type not in _ALLOWED_CONTENT_TYPES:
-                raise ContentTypeNotAllowed(f'{main_type} not in {_ALLOWED_CONTENT_TYPES}')
-
-        title = None
-        size = int(response.headers.get('Content-Length', 0))
-        content_disposition = response.headers.get('Content-Disposition')
-        if content_disposition:
-            _, params = cgi.parse_header(content_disposition)
-            title = params.get('filename')
-        elif content_type in _HTML_MIMETYPES or content_type == 'text/plain':
-            content = _read_stream(response)
-            if content and not size:
-                size = len(content.encode('UTF-8'))
-
-            soup = bs4.BeautifulSoup(content, 'html5lib')
-            if title_tag := soup.find('meta', property='og:title', content=True):
-                title = title_tag.get('content') or soup.title.string
-
-            site_name = None
-            if site_name_tag := soup.find('meta', property='og:site_name', content=True):
-                site_name = site_name_tag.get('content')
-
-            if site_name and len(site_name) < (site_name_max_size := 15):
-                if len(site_name) > site_name_max_size:
-                    site_name = ''.join(title[:site_name_max_size - 3]) + '...'
-                hostname = site_name
-
-            if description_tag := soup.find('meta', property='og:description', content=True):
-                description = description_tag.get('content')
-
-                # GitHub's repo <title> is better than the og:title
-                # How to check if it's a repo? Simple.
-                # The description on GitHub ends with the repo name, AKA the og:title.
-                if site_name == 'GitHub' and title in description:
-                    title = soup.title.string.replace('GitHub - ', '', 1)
-
-                if site_name == 'Twitter' and description:
-                    title = f'{title}: {description}'
-
-            if not title and (content and content_type not in _HTML_MIMETYPES):
-                title = re.sub(r'\s+', ' ', ' '.join(content.split('\n')))
-
-        if title:
-            title = unstyle(html.unescape(title).strip())
-            if len(title) > _MAX_TITLE_LENGTH:
-                title = ''.join(title[:_MAX_TITLE_LENGTH - 3]) + '...'
-
-    return hostname, title, content_type, size
-
-
 def _clean_url(url: str):
     if url:
         url = url.rstrip('\'.,"\1')
-        for left_brace, right_brace in _BRACES:
+        braces = [('{', '}'), ('<', '>'), ('[', ']'), ('(', ')')]
+        for left_brace, right_brace in braces:
             if left_brace not in url and url.endswith(right_brace):
                 url = url.rstrip(right_brace)
     return url
@@ -174,20 +64,41 @@ class UrlInfo(object):
         'plugins.botui'
     ]
 
+    _max_bytes = 10 * 1024 * 1024  # 10M
+    _url_regex = re.compile(r'(?:https?://\S+)', re.IGNORECASE | re.UNICODE)
+    _hostname_cleanup_regex = re.compile(r'^www\.', re.IGNORECASE | re.UNICODE)
+    _max_title_length = 128
+    _request_timeout = 5
+    _html_mimetypes = ['text/html', 'application/xhtml+xml']
+    _request_chunk_size = 1024  # Bytes
+    _allowed_content_types = ['text', 'video', 'application']
+
     def __init__(self, bot):
         self.bot = bot
         self.config = self.bot.config.get(__name__, {})
         self.ignore_nicks = self.config.get('ignore_nicks', '').split()
         self.ignore_hostnames = self.config.get('ignore_hostnames', '').split()
-        socket.getaddrinfo = _getaddrinfo_wrapper
+        self.ipv4_hostnames = self.config.get('ipv4_hostnames', '').split()
+        self._original_getaddrinfo = socket.getaddrinfo
+        socket.getaddrinfo = self._getaddrinfo_wrapper
 
-    @irc3.event(r':(?P<mask>\S+!\S+@\S+) PRIVMSG (?P<target>#\S+) :(?iu)(?P<data>.*{0}).*'.format(_URL_FINDER.pattern))
+    def _getaddrinfo_wrapper(self, host, port, family=0, type=0, proto=0, flags=0):
+        """
+        Some sites like YouTube like to block certain IPv6 ranges, so forcing IPv4 is necessary to get info on those URLs.
+        Because neither requests, urllib, or HTTPRequest provide a way to do that, it's necessary to bypass them and
+        go straight to the Python socket library and wrap it's getaddrinfo function transparently to return only
+        IPv4 addresses for certain hosts.
+        """
+        family = socket.AF_INET if any(host.endswith(hostname) for hostname in self.ipv4_hostnames) else family
+        return self._original_getaddrinfo(host, port, family, type, proto, flags)
+
+    @irc3.event(rf':(?P<mask>\S+!\S+@\S+) PRIVMSG (?P<target>#\S+) :(?iu)(?P<data>.*{_url_regex.pattern}).*')
     def on_url(self, mask, target, data):
         if mask.nick in self.ignore_nicks or data.startswith(self.bot.config.cmd) or data.startswith(
                 f'{self.bot.nick}: '):
             return
 
-        urls = [_clean_url(url) for url in set(_URL_FINDER.findall(data))] or []
+        urls = [_clean_url(url) for url in set(self._url_regex.findall(data))] or []
         for url in urls:
             if urlparse(url).hostname in self.ignore_hostnames:
                 urls.remove(url)
@@ -202,7 +113,7 @@ class UrlInfo(object):
             messages = []
             self.bot.log.debug(f'Retrieving page titles for {urls}')
 
-            future_to_url = {executor.submit(_process_url, url, self.bot.requests): url for url in urls}
+            future_to_url = {executor.submit(self._process_url, url): url for url in urls}
             for future in concurrent.futures.as_completed(future_to_url):
                 url = future_to_url[future]
                 hostname = urlparse(url).hostname
@@ -240,3 +151,86 @@ class UrlInfo(object):
             if messages:
                 pipe_character = style(' | ', fg=Color.LIGHT_GRAY)
                 self.bot.privmsg(target, pipe_character.join(messages))
+
+    def _stream_response(self, response: requests.Response) -> str:
+        start_time = time.time()
+        content = StringIO()
+
+        for chunk in response.iter_content(self._request_chunk_size):
+            if time.time() - start_time >= self._request_timeout:
+                raise RequestTimeout(f'Request timed out ({self._request_timeout} seconds).')
+            if not chunk:  # filter out keep-alive new chunks
+                continue
+            content_length = content.write(chunk.decode('UTF-8', errors='ignore'))
+            if content_length > self._max_bytes:
+                size = naturalsize(content_length, gnu=True)
+                raise ResponseBodyTooLarge(f'Couldn\'t find the page title within {size}.')
+
+        return content.getvalue()
+
+    def _process_url(self, url: str):
+        hostname = urlparse(url).hostname
+        for (_, _, _, _, sockaddr) in socket.getaddrinfo(hostname, None):
+            ip = ipaddress.ip_address(sockaddr[0])
+            if not ip.is_global:
+                raise InvalidIPAddress(f'{hostname} is not a publicly routable address.')
+
+        hostname = self._hostname_cleanup_regex.sub('', hostname)
+        with self.bot.requests.get(url, stream=True) as response:
+            if response.status_code != requests.codes.ok:
+                response.raise_for_status()
+
+            content_type = response.headers.get('Content-Type')
+            if content_type:
+                content_type, _ = cgi.parse_header(content_type)
+                main_type = content_type.split('/')[0]
+                if main_type not in self._allowed_content_types:
+                    raise ContentTypeNotAllowed(f'{main_type} not in {self._allowed_content_types}')
+
+            title = None
+            size = int(response.headers.get('Content-Length', 0))
+            content_disposition = response.headers.get('Content-Disposition')
+            if content_disposition:
+                _, params = cgi.parse_header(content_disposition)
+                title = params.get('filename')
+            elif content_type in self._html_mimetypes or content_type == 'text/plain':
+                content = self._stream_response(response)
+                if content and not size:
+                    size = len(content.encode('UTF-8'))
+
+                soup = bs4.BeautifulSoup(content, 'html5lib')
+                if title_tag := soup.find('meta', property='og:title', content=True):
+                    title = title_tag.get('content')
+                else:
+                    title = soup.title.string
+
+                site_name = None
+                if site_name_tag := soup.find('meta', property='og:site_name', content=True):
+                    site_name = site_name_tag.get('content')
+
+                if site_name and len(site_name) < (site_name_max_size := 15):
+                    if len(site_name) > site_name_max_size:
+                        site_name = ''.join(title[:site_name_max_size - 3]) + '...'
+                    hostname = site_name
+
+                if description_tag := soup.find('meta', property='og:description', content=True):
+                    description = description_tag.get('content')
+
+                    # GitHub's repo <title> is better than the og:title
+                    # How to check if it's a repo? Simple.
+                    # The description on GitHub ends with the repo name, AKA the og:title.
+                    if site_name == 'GitHub' and title in description:
+                        title = soup.title.string.replace('GitHub - ', '', 1)
+
+                    if site_name == 'Twitter' and description:
+                        title = f'{title}: {description}'
+
+                if not title and (content and content_type not in self._html_mimetypes):
+                    title = re.sub(r'\s+', ' ', ' '.join(content.split('\n')))
+
+            if title:
+                title = unstyle(html.unescape(title).strip())
+                if len(title) > self._max_title_length:
+                    title = ''.join(title[:self._max_title_length - 3]) + '...'
+
+        return hostname, title, content_type, size
