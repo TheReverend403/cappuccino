a63c4f76925f3ac26f21bcded25b8ba1b61311cb
Lee Watson <rev@revthefox.co.uk>
2020-07-31 14:40:49 +0100

    Make sure ircstyle is passed a string.

--- util/formatting.py
+++ util/formatting.py
@@ -37,7 +37,7 @@ class Color:
 
 
 def style(text: str, fg: Color = None, bg: Color = None, bold=False, italics=False, underline=False, reset=True) -> str:
-    return ircstyle.style(text, fg=fg, bg=bg, italics=italics, underline=underline, bold=bold, reset=reset)
+    return ircstyle.style(str(text), fg=fg, bg=bg, italics=italics, underline=underline, bold=bold, reset=reset)
 
 
 def unstyle(text: str) -> str:
