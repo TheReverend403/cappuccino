4e9ea883f8500023c924d0b771276289f2360847
Lee Watson <rev@revthefox.co.uk>
2021-01-05 22:37:17 +0000

    More DRY with truncate_with_ellipsis.

--- cappuccino/urlinfo.py
+++ cappuccino/urlinfo.py
@@ -234,8 +234,7 @@ class UrlInfo(Plugin):
                     site_name = site_name_tag.get("content")
 
                 if site_name and len(site_name) < (site_name_max_size := 15):
-                    if len(site_name) > site_name_max_size:
-                        site_name = "".join(title[: site_name_max_size - 3]) + "..."
+                    site_name = truncate_with_ellipsis(title, site_name_max_size)
                     hostname = site_name
 
                 if description_tag := soup.find(
@@ -257,7 +256,6 @@ class UrlInfo(Plugin):
 
             if title:
                 title = unstyle(html.unescape(title).strip())
-                if len(title) > self._max_title_length:
-                    title = truncate_with_ellipsis(title, self._max_title_length)
+                title = truncate_with_ellipsis(title, self._max_title_length)
 
         return hostname, title, content_type, size
