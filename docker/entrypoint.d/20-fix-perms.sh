3c9ab66814f660ab4282804e65db1e002db62910
Lee Watson <me@lwatson.dev>
2022-01-11 00:08:06 +0000

    Make PUID and PGID optional at the runtime level

--- docker/entrypoint.d/20-fix-perms.sh
+++ docker/entrypoint.d/20-fix-perms.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-echo "Changing UID of cappuccino to $PUID"
-usermod -u "$PUID" cappuccino
-echo "Changing GID of cappuccino to $PGID"
-groupmod -g "$PGID" cappuccino
+if test -z "$PUID" && test -z "$PGID"; then
+    echo "Neither PUID or PGID are set, skipping permission fixes."
+    exit 0
+fi
+
+if test -n "$PUID"; then
+    echo "Changing UID of cappuccino to $PUID"
+    usermod -u "$PUID" cappuccino
+fi
+
+if test -n "$PGID"; then
+    echo "Changing GID of cappuccino to $PGID"
+    groupmod -g "$PGID" cappuccino
+fi
 
 echo "Changing ownership of /config, /data, /app"
 chown -R cappuccino:cappuccino /config /data /app
