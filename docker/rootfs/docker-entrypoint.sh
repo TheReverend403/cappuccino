ae2ad4e7fc2836cfc419fd322b173518dea570f2
Lee Watson <me@lwatson.dev>
2024-02-22 19:06:23 +0000

    Switch to curly braces for env vars

--- docker/rootfs/docker-entrypoint.sh
+++ docker/rootfs/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
 
-/docker-env2conf.py "$SETTINGS_SOURCE_FILE" "$SETTINGS_FILE"
-alembic upgrade head
-exec irc3 "$SETTINGS_FILE"
+/docker-env2conf.py "${SETTINGS_SOURCE_FILE}" "${SETTINGS_FILE}"
+
+if [ "${SKIP_MIGRATIONS:-false}" = "false" ]; then
+    alembic upgrade head
+else
+    echo "Skipping migrations due to SKIP_MIGRATIONS=${SKIP_MIGRATIONS}"
+fi
+
+exec irc3 "${SETTINGS_FILE}"
